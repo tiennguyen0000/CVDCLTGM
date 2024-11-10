27064d7c6f1534c92e25a85694f4b7c70d8d4c9f
TienNguyen000 <160461053+tiennguyen0000@users.noreply.github.com>
2024-11-10 14:50:26 +0700

    up backend

---
+++ Backend/routes/base.py
@@ -0,0 +1,6 @@
+from fastapi import APIRouter
+from .catdog_route import router as catdog_cls_route
+
+router = APIRouter()
+router.include_router(catdog_cls_route, prefix="/catdog_classification")
+
