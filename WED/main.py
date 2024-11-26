493322105e57507573fe3b7685d72101395485b0
TienNguyen000 <160461053+tiennguyen0000@users.noreply.github.com>
2024-11-26 23:31:29 +0700

    fix

--- WED/main.py
+++ WED/main.py
@@ -3,8 +3,8 @@ import torch
 from PIL import Image
 from diffusers.utils.torch_utils import randn_tensor
 
-from src.config import RunConfig
-from src.utils.enums_utils import model_type_to_size, is_stochastic
+from WED.model.config import RunConfig
+from WED.model.Utils.enums_utils import model_type_to_size, is_stochastic
 
 def create_noise_list(model_type, length, generator=None):
     img_size = model_type_to_size(model_type)
